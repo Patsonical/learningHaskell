649368d4512d22065dab48f2c817467c4c74cbe5
Patryk K <patryk.kaczmarczyk.mail@gmail.com>
2019-05-09 22:15:20 +0100

    Figured out BST deletion (I think!); implemented delete, find and getVal functions

--- binaryTrees.hs
+++ binaryTrees.hs
@@ -1,6 +1,9 @@
         -- Declaration
 data Tree a = NIL | Branch a (Tree a) (Tree a) deriving (Show)
 
+getVal :: Tree a -> a
+getVal (Branch val _ _) = val
+
         -- Test tree
 testTree = Branch 7 (Branch 5 (Branch 2 NIL NIL) (Branch 6 NIL NIL)) (Branch 10 (Branch 8 NIL NIL) NIL)
 
@@ -31,11 +34,34 @@ maxTree (Branch val _ right) = maxTree right
         -- BST Insertion
 insert :: (Ord a) => a -> Tree a -> Tree a
 insert ins NIL = Branch ins NIL NIL
-insert ins (Branch val left right)
-        | ins == val    = Branch val left right
+insert ins this@(Branch val left right)
+        | ins == val    = this
         | ins < val     = Branch val (insert ins left) right
         | ins > val     = Branch val left (insert ins right)
 
+insertMultiple :: (Ord a) => [a] -> Tree a -> Tree a
+insertMultiple [] tree = tree
+insertMultiple (x:xs) tree = insertMultiple xs (insert x tree)
+
+        -- BST Find
+find :: (Ord a) => a -> Tree a -> Tree a
+find item NIL = NIL
+find item this@(Branch val left right)
+        | item == val   = this
+        | item < val    = find item left
+        | item > val    = find item right
+
         -- BST Deletion
+
+del :: (Ord a) => Tree a -> Tree a
+del (Branch val left NIL)       = left
+del (Branch val NIL right)      = right
+del (Branch val left right)     = Branch min left (delete min right)
+        where min = getVal (minTree right)
+
 delete :: (Ord a) => a -> Tree a -> Tree a
-delete del NIL = NIL
+delete item NIL = NIL
+delete item this@(Branch val left right)
+        | item == val   = del this
+        | item < val    = Branch val (delete item left) right
+        | item > val    = Branch val left (delete item right)
